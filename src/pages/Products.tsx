2d04b9d56840318b15fe6b1356b47c5e6c73ca75
pgautame <59304773+pgautame@users.noreply.github.com>
2024-07-29 12:11:15 +0530

    added sidebar, widgets, chart and table

---
+++ src/pages/Products.tsx
@@ -0,0 +1,12 @@
+import AdminSidebar from "../components/AdminSidebar";
+
+const Products = () => {
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main></main>
+    </div>
+  );
+};
+
+export default Products;
---
+++ src/pages/Transaction.tsx
@@ -0,0 +1,12 @@
+import AdminSidebar from "../components/AdminSidebar";
+
+const Transaction = () => {
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main></main>
+    </div>
+  );
+};
+
+export default Transaction;
